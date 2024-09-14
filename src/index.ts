e07d92707170876bb0c231200717309034a35a70
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-09-14 19:38:02 +0900

    feat: Get page title

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { Builder } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
+
+export const handler = async () => {
+    const options = new chrome.Options();
+    options.addArguments('--headless=new');
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-dev-shm-usage');
+    options.addArguments('--disable-gpu');
+    options.addArguments('--disable-dev-tools');
+    options.addArguments('--no-zygote');
+    options.addArguments('--single-process');
+    options.addArguments('--remote-debugging-pipe');
+    options.addArguments('--window-size=1920,1080');
+
+    options.setBinaryPath('/opt/chrome/chrome-linux64/chrome');
+
+    const serviceBuilder = new chrome.ServiceBuilder('/opt/chrome-driver/chromedriver-linux64/chromedriver');
+
+    const driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .setChromeService(serviceBuilder)
+        .build();
+
+    try {
+        console.log('Navigating to Website...');
+        await driver.get("https://www.google.com/")
+    
+        const title = await driver.getTitle();
+        console.log(`Page title is: ${title}`);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    } finally {
+        console.log('Quitting the driver...');
+        await driver.quit();
+        console.log('Driver quit successfully.');
+    }
+};
+
